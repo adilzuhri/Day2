1b91c5a110bc4aaa69be0e626c9b107112f44549
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-07-01 01:13:41 +0800

    First commit

---
+++ Day02/Arr.java
@@ -0,0 +1,14 @@
+package Day02;
+
+public class Arr {
+
+    public static void main(String[] args) {
+
+        // for loop
+        for (int idx = 0; idx < args.length; idx++) {
+            System.out.printf("idx = %d\n", idx);
+        }
+
+    }
+    
+}
